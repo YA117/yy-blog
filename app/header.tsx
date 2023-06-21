801463032f39b577b55198d6b9ea2dc4a8f1d77e
Yusuf A Y <51889749+YA117@users.noreply.github.com>
2023-06-21 10:51:18 +0100

    Update header.tsx

--- app/header.tsx
+++ app/header.tsx
@@ -21,8 +21,7 @@ export function Header() {
           target="_blank"
           className="inline-flex hover:bg-gray-200 dark:hover:bg-[#313131] active:bg-gray-300 dark:active:bg-[#242424] items-center p-2 rounded-sm transition-[background-color] whitespace-nowrap -mr-2"
         >
-          <TweetIcon style={{ marginRight: 4 }} />{" "}
-          <span className="hidden md:inline">&nbsp;</span>
+          <TweetIcon style={{ marginRight: 4 }} />
         </a>
       </nav>
     </header>
